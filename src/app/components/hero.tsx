da59f315e5199bd510aa5254c607df45b6338f90
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-03-18 21:00:38 +0530

    installation and setup
    installation and setup of hero section

---
+++ src/app/components/hero.tsx
@@ -0,0 +1,48 @@
+"use client"
+import React, { useRef } from 'react'
+import { motion , useScroll , useTransform } from "framer-motion";
+
+
+const Hero = () => {
+    const ref = useRef(null);   
+    const { scrollYProgress } = useScroll({
+        target: ref,
+        offset: ["start start", "end start"],
+    });
+
+    const textY = useTransform(scrollYProgress , [0, 1] ,["10%", "900%"]);
+
+
+  return (
+    <div 
+    ref={ref}
+    className='w-full h-screen overflow-hidden relative place-items-center'>
+  
+      <motion.h1 
+      style={{y: textY}}
+      className='font-bold text-white text-6xl relative z-10 pt-13 '>
+         WELCOME
+      </motion.h1>
+      
+      <motion.div
+      className='absolute inset-0 z-0'
+      style={{
+          backgroundImage : `url(/sky.jpg)`,
+          backgroundPosition : "center bottom 30%",
+          backgroundSize : "cover"
+      }}
+      />
+  
+  <div
+      className='absolute inset-0 z-20'
+      style={{
+          backgroundImage : `url(/mountains.png)`,
+          backgroundPosition : "bottom",
+          backgroundSize : "cover"
+      }}
+      />
+    </div>
+  )
+}
+
+export default Hero
