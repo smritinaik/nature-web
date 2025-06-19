576bf074e4ebd0cf95f80b0d99c9f3d6e3362c5d
smritinaik <148971067+smritinaik@users.noreply.github.com>
2025-06-19 20:03:55 +0530

    formating
    formating

--- src/app/components/hero.tsx
+++ src/app/components/hero.tsx
@@ -1,22 +1,22 @@
 "use client"
 import React, { useRef } from 'react'
-import { motion , useScroll , useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 
 const Hero = () => {
-    const ref = useRef(null);   
-    const { scrollYProgress } = useScroll({
-        target: ref,
-        offset: ["start start", "end start"],
-    });
+  const ref = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: ref,
+    offset: ["start start", "end start"],
+  });
 
-    const sunY = useTransform(scrollYProgress , [0, 1] ,["10%", "990%"]);
+  const sunY = useTransform(scrollYProgress, [0, 1], ["10%", "990%"]);
 
 
   return (
-    <div 
-    ref={ref}
-    className='w-full h-screen overflow-hidden justify-center relative place-items-center'>
+    <div
+      ref={ref}
+      className='w-full h-screen overflow-hidden justify-center relative place-items-center'>
 
       <motion.img
         src="/fsun.png"
@@ -24,23 +24,23 @@ const Hero = () => {
         style={{ y: sunY }}
         className='absolute z-10 top-8 left-1/2 transform -translate-x-1/2 w-45 h-45'
       />
-      
+
       <motion.div
-      className='absolute inset-0 z-0'
-      style={{
-          backgroundImage : `url(/download.jpg)`,
-          backgroundPosition : "center bottom 40%",
-          backgroundSize : "cover"
-      }}
+        className='absolute inset-0 z-0'
+        style={{
+          backgroundImage: `url(/download.jpg)`,
+          backgroundPosition: "center bottom 40%",
+          backgroundSize: "cover"
+        }}
       />
-  
-  <div
-      className='absolute inset-0 z-20 '
-      style={{
-          backgroundImage : `url(/fmountain.png)`,
-          backgroundPosition : "bottom",
-          backgroundSize : "cover"
-      }}
+
+      <div
+        className='absolute inset-0 z-20 '
+        style={{
+          backgroundImage: `url(/fmountain.png)`,
+          backgroundPosition: "bottom",
+          backgroundSize: "cover"
+        }}
       />
     </div>
   )
